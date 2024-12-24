a26d3d78fedabe5683d4f86225e565b1e9e18a25
Sapthesh V <sapthesh@users.noreply.github.com>
2024-12-24 08:32:30 +0530

    Add files via upload

---
+++ routes/reviews.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true});
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+const reviews = require('../controllers/reviews');
+const { reviewSchema} = require('../schemas');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+
+
+
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.newReview));
+
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
+
+module.exports = router;
